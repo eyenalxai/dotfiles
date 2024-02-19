a4f9e4b2a4a9ea7e4096da1245809d96f20b3e25
Sax Dhiut <mail@iyxeyl.me>
2024-02-19 17:19:12 +0400

    Fix my stupidness

---
+++ .config/zsh/os_specific.zsh
@@ -0,0 +1,6 @@
+EDITOR=/usr/bin/helix
+
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+source /usr/share/fzf/key-bindings.zsh
