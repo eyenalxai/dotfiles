86681bbb40e3601a430b1efd8022668e719d2b8f
Sax Dhiut <mail@iyxeyl.me>
2024-02-19 17:19:12 +0400

    Fix my stupidness

---
+++ .config/zsh/shared.zsh
@@ -0,0 +1,27 @@
+export ZSH="${HOME}/.oh-my-zsh"
+
+# Plugins
+plugins=(
+    alias-finder
+    git
+    history-substring-search
+)
+
+# Disable zsh substitution/autocomplete with URL and backslashes
+DISABLE_MAGIC_FUNCTIONS=true
+
+# Activate
+source $ZSH/oh-my-zsh.sh
+
+# Starship Prompt
+eval "$(starship init zsh)"
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
+
+# History
+source "${HOME}/.config/zsh/history.zsh"
+
+# FZF
+export FZF_DEFAULT_COMMAND='rg --files --hidden -g "!.git" '
+
+# Zoxide
+eval "$(zoxide init zsh)"
