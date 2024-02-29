f81c4afca131b2489be93f2d07b0fc1bc7ccdbf3
Sax Dhiut <mail@iyxeyl.me>
2024-02-29 15:28:49 +0400

    Who the hell uses 8-space tabs?

--- .config/zsh/os_specific.zsh
+++ .config/zsh/os_specific.zsh
@@ -4,3 +4,11 @@ source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 source /usr/share/fzf/key-bindings.zsh
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# SSH Agent
+SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gcr/ssh"
