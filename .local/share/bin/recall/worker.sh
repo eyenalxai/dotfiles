0d6876a25fdd379a78d5516b543465bb279eb12d
Sax Dhiut <mail@iyxeyl.me>
2024-05-25 13:48:57 +0400

    You are not expected to understand this

--- .local/share/bin/recall/worker.sh
+++ .local/share/bin/recall/worker.sh
@@ -15,3 +15,10 @@ tesseract "${SCREENSHOT_FILE}" "${TEXT_FILE}"
 
 echo "Screenshot saved to: ${SCREENSHOT_FILE}"
 echo "Text extracted to: ${TEXT_FILE}"
+
+DAYS_OLD=30
+
+SCREENSHOTS_DELETED=$(find "${SCREENSHOTS_DIR}" -type f -name '*.png' -mtime +${DAYS_OLD} -exec rm {} \; -print | wc -l)
+TEXTS_DELETED=$(find "${TEXTS_DIR}" -type f -mtime +${DAYS_OLD} -exec rm {} \; -print | wc -l)
+
+echo "${SCREENSHOTS_DELETED} screenshot(s) and ${TEXTS_DELETED} text file(s) older than ${DAYS_OLD} days were deleted."
