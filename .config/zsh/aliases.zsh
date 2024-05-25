704a381a206be3de5bd2cb66378985949214c1f4
Sax Dhiut <mail@iyxeyl.me>
2024-05-25 13:50:18 +0400

    Sucks, but what are you gonna do

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias hx=helix
+alias reboot-into-windows="systemctl reboot --boot-loader-entry=windows.conf"
+alias yaas="yay -S --noconfirm"
+alias yaasu="yay -Suy --noconfirm"
+alias recall="bash ${HOME}/.local/share/bin/recall/recall.sh"
