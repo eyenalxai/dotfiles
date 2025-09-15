d6a0d001c47650e449c7e5ab4e50035c431e8fdf
Midliv Tucym <mail@iyxeyl.me>
2025-09-15 07:24:44 +0400

    chore: test

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,4 @@
-alias hx=helix
-alias reboot-into-windows="systemctl reboot --boot-loader-entry=windows.conf"
+alias n=nvim
 alias yaas="yay -S --noconfirm"
 alias yaasu="yay -Suy --noconfirm"
-alias recall="bash ${HOME}/.local/share/bin/recall/recall.sh"
+
