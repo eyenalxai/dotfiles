4ef53037a470b6950491533b78ae388373dbe37b
Midliv Tucym <mail@iyxeyl.me>
2025-09-15 07:24:14 +0400

    perf: NO COMMENT

--- .config/zsh/os_specific.zsh
+++ .config/zsh/os_specific.zsh
@@ -1,14 +1,8 @@
-EDITOR=/usr/bin/helix
+EDITOR=/usr/bin/nvim
 
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 source /usr/share/fzf/key-bindings.zsh
-
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
 
 export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
