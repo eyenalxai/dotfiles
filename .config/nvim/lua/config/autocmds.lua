ceb18445237d9b9fd2f9a94eb5f10967be0ab016
Sax Dhiut <mail@iyxeyl.me>
2025-11-27 10:03:32 +0400

    fix: whatever

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,6 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable LazyVim's default wrap+spell autocmds so spell never auto-enables
+pcall(vim.api.nvim_del_augroup_by_name, "lazyvim_wrap_spell")
