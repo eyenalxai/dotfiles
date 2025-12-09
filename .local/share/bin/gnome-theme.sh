6772f6d7ac3a7e8485b83ae53f09f684fd481283
Sax Dhiut <mail@iyxeyl.me>
2025-12-09 17:28:09 +0400

    refactor: every line of code is written by God and under the laws of physics, the code must move forward

---
+++ .local/share/bin/gnome-theme.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
+
+sudo gtk-update-icon-cache /usr/share/icons/Yaru
