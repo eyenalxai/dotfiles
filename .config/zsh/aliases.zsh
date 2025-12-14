f9cb36fc97497c6efb83efa50294f0b449e074c2
Sax Dhiut <mail@iyxeyl.me>
2025-12-14 10:28:36 +0400

    feat: damn. fixed

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,3 +5,5 @@ alias yaas="yay -S"
 alias yaasu="sudo pacman -Suy --noconfirm && yay -Suy"
 alias yaac="yay -Rscnd $(yay -Qdtq | xargs)"
 
+alias json-schema="jq --argjson nullable true 'include \"schema\"; schema'"
+
