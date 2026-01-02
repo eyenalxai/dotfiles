baa1df47e466889e69132e285cc60ba1893486f0
Sax Dhiut <mail@iyxeyl.me>
2026-01-02 17:50:07 +0400

    build: ugly but it works

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias n=nvim
 alias node-n='command n'
 
 alias yaas="yay -S"
-alias yaasu="sudo pacman -Suy --noconfirm && yay -Suy && yay -Rscnd $(yay -Qdtq)"
+alias yaasu="sudo pacman -Suy --noconfirm && yay -Suy --noconfirm"
 alias yaac="yay -Rscnd $(yay -Qdtq | xargs)"
 
 alias json-schema="jq --argjson nullable true 'include \"schema\"; schema'"
