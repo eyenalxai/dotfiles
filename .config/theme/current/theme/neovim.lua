a4aef5ac7e76a7670707412d1e03d60ba7aaade6
Sax Dhiut <mail@iyxeyl.me>
2026-01-15 22:50:54 +0400

    chore: LMAO

--- .config/theme/current/theme/neovim.lua
+++ .config/theme/current/theme/neovim.lua
@@ -1,13 +1,9 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-	},
+	{ "ellisonleao/gruvbox.nvim" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "gruvbox",
 		},
 	},
 }
