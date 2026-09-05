6e5135d7ff7281807640a7a9cf20b0a6d54ee2d9
Sax Dhiut <mail@iyxeyl.me>
2026-09-05 11:56:36 +0400

    feat(zsh): drop oh-my-zsh in favor of native compinit and omarchy env

--- .config/zsh/shared.zsh
+++ .config/zsh/shared.zsh
@@ -1,25 +1,17 @@
-export ZSH="${HOME}/.oh-my-zsh"
-
-# Plugins
-plugins=(
-    alias-finder
-    bun
-    git
-    history-substring-search
-)
-
-# Disable zsh substitution/autocomplete with URL and backslashes
-DISABLE_MAGIC_FUNCTIONS=true
-
-# Activate
-source $ZSH/oh-my-zsh.sh
+# Basic completion setup
+autoload -Uz compinit
+compinit
 
 # Aliases
 source "${HOME}/.config/zsh/aliases.zsh"
 
 # Starship Prompt
 eval "$(starship init zsh)"
-export STARSHIP_CONFIG=~/.config/starship/starship.toml
+if [[ -f "${HOME}/.config/starship/starship.toml" ]]; then
+  export STARSHIP_CONFIG="${HOME}/.config/starship/starship.toml"
+elif [[ -f "${HOME}/.config/starship.toml" ]]; then
+  export STARSHIP_CONFIG="${HOME}/.config/starship.toml"
+fi
 
 # History
 source "${HOME}/.config/zsh/history.zsh"
--- .zshrc
+++ .zshrc
@@ -1,2 +1,4 @@
+[[ -r /usr/share/omarchy/default/bash/env-bootstrap ]] && source /usr/share/omarchy/default/bash/env-bootstrap
+
 source "${HOME}/.config/zsh/shared.zsh"
 source "${HOME}/.config/zsh/os_specific.zsh"
